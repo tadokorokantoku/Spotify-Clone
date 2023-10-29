2fccf4a26a79fe17c4b0837635ddc55184bfe083
INO <katsuki104@gmail.com>
2023-10-29 12:10:24 +0900

    メイン画面のページングボタン

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,19 @@
+import Header from '@/components/Header'
+
 export default function Home() {
   return (
-    <div className="text-green-500">Hello Spotify Again!</div>
+    <div className="
+    text-green-500
+    bg-neutral-900
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overflow-y-auto
+    ">
+      <Header>
+        Heder
+      </Header>
+    </div>
   )
 }
