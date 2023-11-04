65863e59f5370d31eee5a7197f8d11e3a23a36a1
INO <katsuki104@gmail.com>
2023-11-04 14:07:36 +0900

    ほっとリロード用の設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 200,
+      };
+    }
+
+    return config;
+  },
+}
 
 module.exports = nextConfig
