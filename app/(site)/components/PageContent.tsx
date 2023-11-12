b96c155ae37a53805dc4b063349a2517bd2a67ff
INO <katsuki104@gmail.com>
2023-11-12 18:34:14 +0900

    ログインしていないとライブラリが出ないようにする

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -6,6 +6,7 @@ import { AiOutlinePlus } from 'react-icons/ai';
 
 import useSearchModal from '@/hooks/useSearchModal';
 import Best10Songs from '@/domain/Best10Songs/Best10Songs';
+import { useUser } from '@/hooks/useUser';
 
 
 interface PageContentProps {
@@ -15,6 +16,8 @@ interface PageContentProps {
 const PageContent: FC<PageContentProps> = ({
   songs,
 }) => {
+  const user = useUser();
+  console.log('user', user)
   const searchModal = useSearchModal();
   const canRegister = songs.length < 10 
 
@@ -27,38 +30,43 @@ const PageContent: FC<PageContentProps> = ({
   }
   return (
     <div className='mt-2 mb-7 px-6'>
-      <div className='flex justify-between items-center'>
-        <div className='flex gap-10'>
-          <div className='text-white text-2xl font-semibold'>
-            Your 10 songs
-          </div>
-          {canRegister && (
-            <>
-              <div>
-                <button
-                  className='
-                    bg-blue-500
-                    rounded-full
-                    p-2
-                    hover:bg-blue-600
-                    transition
-                  '
-                  onClick={searchModal.onOpen}
-                  disabled={songs.length >= 10}
-                >
-                  <AiOutlinePlus className='text-white' />
-                </button>
+      {user.user && (
+        <>
+          <div className='flex justify-between items-center'>
+            <div className='flex gap-10'>
+              <div className='text-white text-2xl font-semibold'>
+                Your 10 songs
               </div>
-              <p className='text-center p-1'>
-                { canRegister && `あと${10 - songs.length}曲登録することができます`}
-              </p>
-            </>
-          )}
-        </div>
-      </div>
-      <div className='mt-10'>
-        <Best10Songs songs={songs} />
-      </div>
+              {canRegister && (
+                <>
+                  <div>
+                    <button
+                      className='
+                        bg-blue-500
+                        rounded-full
+                        p-2
+                        hover:bg-blue-600
+                        transition
+                      '
+                      onClick={searchModal.onOpen}
+                      disabled={songs.length >= 10}
+                    >
+                      <AiOutlinePlus className='text-white' />
+                    </button>
+                  </div>
+                  <p className='text-center p-1'>
+                    { canRegister && `あと${10 - songs.length}曲登録することができます`}
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
+      
+          <div className='mt-10'>
+            <Best10Songs songs={songs} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
